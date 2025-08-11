f2fc2bde65939cfadf8d431298eb02d5c518cf33
sera <sera9898@naver.com>
2025-08-11 13:58:25 +0900

    ㅅㅈ

--- src/data/ermitageArtworks.js
+++ src/data/ermitageArtworks.js
@@ -1,117 +1,166 @@
 export const ermitageArtworks  = [
   {
     id: 0,
-    image: '/img/image (1).png',
+    image: '/img/ermitage/img1.png',
     title: 'Return of the Prodigal Son',
-    info: '렘브란트 (1606~1669)',
-    description: `Title: 돌아온 탕자 (Return of the Prodigal Son)
-      Artist: 렘브란트 (1606~1669)
-      Date: 1668년경, 네덜란드
-      Dimensions: 262×205 cm
-      Classification: 유화`,
-    story: ['누가복음의 탕자의 귀환 장면. 아들을 포용하는 아버지의 두 손은 각각 남성과 여성의 손을 형상화해 상징성을 더함 박물관에서 이 그림을 안 보고 나오면 바보라는 말까지 있을 정도로 유명한 작품']
+    info: 'Rembrandt Harmenszoon van Rijn, 1661~1669',
+    descriptions: [
+      'Title : 돌아온 탕자 (De terugkeer van de verloren zoon)',
+      'Artist : Rembrandt Harmenszoon van Rijn (1606~1669)',
+      'Date : 1668년경, 네덜란드',
+      'Dimensions : 262×205 cm',
+      'Classification : 유화',
+    ],
+    story: [
+      '누가복음의 탕자의 귀환 장면. 아들을 포용하는 아버지의 두 손은 각각 남성과 여성의 손을 형상화해 상',
+      '징성을 더함 박물관에서 이 그림을 안 보고 나오면 바보라는 말까지 있을 정도로 유명한 작품',
+    ]
   },
   {
     id: 1,
-    image: '/img/image (2).png',
+    image: '/img/ermitage/img2.png',
     title: 'In the Garden',
-    info: 'Henri Matisse, 1908년',
-    description: `Title:  In the Garden – 르누아르
-      Artist: 피에르 오귀스트 르누아르 (Pierre-Auguste Renoir)
-      Date: 1668년경, 네덜란드
-      Dimensions: 170.5 × 112.5 cm
-      Classification:유화 `
+    info: 'Pierre Auguste Renoir, 1668',
+    descriptions: [
+      'Title :  In the Garden – 르누아르',
+      'Artist : 피에르 오귀스트 르누아르 (Pierre-Auguste Renoir)',
+      'Date : 1668년경, 네덜란드',
+      'Dimensions : 170.5 × 112.5 cm',
+      'Classification : 유화',
+    ]
   },
   {
   id: 2,
-  image: '/img/image (3).png',
+  image: '/img/ermitage/img3.png',
   title: 'Game of Bowls ',
-  info: 'Henri Matisse. 1909–1910)',
-  description: `Title: Game of Bowls 
-      Artist: Henri Matisse
-      Date: 1909–1910
-      Dimensions: 115 × 147 cm `,
-    story: ['장식용 공놀이 장면. 마티스의 가족을 모델로 하여 원시성과 인간 본성, 신비로움을 표현. 삶의 코드를 해독하는 도구로서의 예술적 게임을 묘사한 깊은 작품.']
+  info: 'Henri Matisse, 1908',
+  descriptions: [
+      'Title : Game of Bowls' ,
+      'Artist : Henri Matisse',
+      'Date : 1908',
+      'Dimensions : 115 × 147 cm',
+    ],
+    story: [
+      '장식용 공놀이 장면. 마티스의 가족을 모델로 하여 원시성과 인간 본성, 신비로움을 표현. 삶의 코드를',
+      '해독하는 도구로서의 예술적 게임을 묘사한 깊은 작품.'
+    ]
+
   }
 ,
   {
     id: 3,
-  image: '/img/image (4).png',
+  image: '/img/ermitage/img4.png',
   title: 'Dance ',
-  info: 'Henri Matisse. 1909–1910)',
-  description: `Title: Dance 
-      Artist: Henri Matisse
-      Date: 1909–1910
-      Dimensions: 260 × 391 cm `,
-    story: ['인간의 잠재의식과 우주의 리듬을 표현한 대표작. 다섯 인물이 원형을 이루며 춤을 추는 모습은 코스모스와의 조화를 상징.']
+  info: 'Henri Matisse, 1909–1910',
+  descriptions: [
+      'Title : Dance' ,
+      'Artist : Henri Matisse',
+      'Date : 1909–1910',
+      'Dimensions : 260 × 391 cm',
+      ],
+    story: [
+      '인간의 잠재의식과 우주의 리듬을 표현한 대표작. 다섯 인물이 원형을 이루며 춤을 추는 모습은 코스모',
+      '스와의 조화를 상징.'
+    ]
   }
 ,
   {
     id: 4,
-    image: '/img/image (5).png',
+    image: '/img/ermitage/img5.png',
     title: ' Reception of the French Ambassador in Venice',
-    info: 'Canaletto , 1726~1727년',
-    description: `Title: Reception of the French Ambassador in Venice 
-      Artist: Canaletto 
-      Date: 1726~1727년
-      Dimensions:181 × 259.5 cm `
+    info: 'Canaletto, 1726~1727',
+    descriptions: [
+      'Title : Reception of the French Ambassador in Venice' ,
+      'Artist : Canaletto' ,
+      'Date : 1726~1727년',
+      'Dimensions : 181 × 259.5 cm',
+    ]
   }
 ,
   {
     id: 5,
-    image: '/img/image (6).png',
+    image: '/img/ermitage/img6.png',
     title: 'Boulevard Montmartre in Paris',
-    info: 'Camille Pissarro,  1897년',
-    description: `Title: Boulevard Montmartre in Paris
-      Artist: Camille Pissarro
-      Date:  1897년 피사로
-      Dimensions: 74 × 92.8 cm `,
-    story: ['몽마르트 대로를 다양한 시간대와 날씨에서 묘사한 도시 풍경화..']
+    info: 'Camille Pissarro, 1897',
+    descriptions: [
+      'Title : Boulevard Montmartre in Paris',
+      'Artist : Camille Pissarro',
+      'Date :  1897년',
+      'Dimensions : 74 × 92.8 cm',
+    ],
+    story: ['몽마르트 대로를 다양한 시간대와 날씨에서 묘사한 도시 풍경화']
   }
   ,
   {
     id: 6,
-    image: '/img/image (7).png',
+    image: '/img/ermitage/img7.png',
     title: 'Madonna and the Child (The Litta Madonna) ',
-    info: 'Leonardo da Vinci . 42 × 33 cm',
-    description: `Title: Madonna and the Child (The Litta Madonna) 
-      Artist: Leonardo da Vinci 1452~1519
-      Date: 1490~1491
-      Dimensions: 42 × 33 cm `
+    info: 'Leonardo da Vinci, 1490~1491',
+    descriptions: [
+      'Title : Madonna and the Child (The Litta Madonna)' ,
+      'Artist : Leonardo da Vinci 1452~1519',
+      'Date : 1490~1491',
+      'Dimensions : 42 × 33 cm',
+    ]
   }
 ,
   {
     id: 7,
-    image: '/img/image (8).png',
-    title: ' Madonna and the Child (The Benois Madonna',
-    info: 'Leonardo da Vinci, 1478년 Italy',
-    description:`Title:  Madonna and the Child (The Benois Madonna
-      Artist: Leonardo da Vinci 1452~1519
-      Date: 1478년 Italy
-      Dimensions:  49.5 × 33 cm`
+    image: '/img/ermitage/img8.png',
+    title: ' Madonna and the Child (The Benois Madonna)',
+    info: 'Leonardo da Vinci, 1478',
+    descriptions:[
+      'Title :  Madonna and the Child (The Benois Madonna)',
+      'Artist : Leonardo da Vinci 1452~1519',
+      'Date : 1478년 Italy',
+      'Dimensions :  49.5 × 33 cm',
+    ]
   },
   {
     id: 8,
-    image: '/img/image (9).png',
-    title: ' Stolen Kiss ',
-    info: '프라고나르 ,1790년대',
-    description: `Title: Stolen Kiss
-      Artist: 라파엘로 
-      Date: 1790년대
-      Dimensions:  45 x 55 cm`,
-      story:['- 로코코 양식의 정수 Fragonard의 *The Stolen Kiss*는 네덜란드 회화의 영향을 받은 정교한 표현과 로코코 특유의 우아한 일상 묘사가 결합된 작품이다.']
+    image: '/img/ermitage/img9.png',
+    title: 'Holy Family',
+    info: 'Raffaello Sanzio da Urbino,  1505~1507 ',
+    descriptions: [
+      'Title : Madonna with Beardless Joseph',
+      'Artist : Raffaello Sanzio da Urbino',
+      'Date :  1505~1507 , Italy',
+      'Dimensions : 72.5 × 56.5 cm',
+      'Classification : 유화',
+    ]
   },
   {
     id: 9,
-    image: '/img/image (10).png',
+    image: '/img/ermitage/img10_1.png',
+    title: ' Stolen Kiss ',
+    info: 'Jean-Honoré Fragonard, 1790',
+    story:[
+      '- 로코코 양식의 정수 Fragonard의 *The Stolen Kiss*는 네덜란드 회화의 영향을 받은 정교한 표현과 ',
+      '로코코 특유의 우아한 일상 묘사가 결합된 작품이다.',
+      '',
+      '아래 그림은 프리고나르의 유명한 `그네`라는 작품이다',
+    ],
+    descriptions: [
+      'Title : Stolen Kiss',
+      'Artist : Jean-Honoré Fragonard' ,
+      'Date : 1790년대',
+      'Dimensions :  45 x 55 cm',
+    ],
+    img5: '/img/ermitage/img10_2.jpg',
+  },
+  {
+    id: 10,
+    image: '/img/ermitage/img11.png',
     title: 'Madonna and Child, with St John The Baptist and Two Angels Making Music',
-    info: 'Lorenzo di Credi, ca.  1510~1520 ',
-    description: `Title: Madonna and Child, with St John The Baptist and Two Angels Making Music
-      Artist: Lorenzo di Credi
-      Date:  1510~1520 Italy
-      Dimensions:  직경 127cm
-      Classification:유화 `
-  }
+    info: 'Lorenzo di Credi, 1510~1520 ',
+    descriptions: [
+      'Title : Madonna and Child, with St John The Baptist and Two Angels Making Music',
+      'Artist : Lorenzo di Credi',
+      'Date :  1510~1520 , Italy',
+      'Dimensions : x 127cm',
+      'Classification : 유화',
+    ]
+  },
 ];
 
 
